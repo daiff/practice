75ec3add206e59f72f27f80145100667873fd441
daiff <605117716@qq.com>
2018-01-04 10:51:03 +0800

    多选框

--- app/src/main/java/com/example/administrator/myapplication/checkbox.java
+++ app/src/main/java/com/example/administrator/myapplication/checkbox.java
@@ -2,6 +2,7 @@ package com.example.administrator.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -12,10 +13,12 @@ import android.widget.TextView;
  */
 
 public class checkbox extends Activity{
+    private static final String TAG = "checkbox";
     private CheckBox one;
     private CheckBox two;
     private CheckBox three;
     private TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +32,50 @@ public class checkbox extends Activity{
         two = (CheckBox) findViewById(R.id.checkBox2);
         three = (CheckBox) findViewById(R.id.checkBox3);
         text = (TextView) findViewById(R.id.text_check);
-        check c = new check();
-        one.setOnClickListener(c);
+
+        checkchange c = new checkchange();
+        one.setOnCheckedChangeListener(c);
+        two.setOnCheckedChangeListener(c);
+        three.setOnCheckedChangeListener(c);
 
     }
-    class check implements CompoundButton.OnCheckedChangeListener{
 
+    class checkchange implements CompoundButton.OnCheckedChangeListener{
+        StringBuilder stringBuilder = new StringBuilder("数字是：");
+        String str;
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            switch(buttonView.getId()){
+                case R.id.checkBox:
+                    str = "111 ";
+                    chnage1(str,isChecked);
+                    break;
+                case R.id.checkBox2:
+                    str = "222 " ;
+                    chnage1(str,isChecked);
+                    break;
+                case R.id.checkBox3:
+                    str = "333 " ;
+                    chnage1(str,isChecked);
+                    break;
+            }
+            text.setText(stringBuilder.toString());
+        }
 
+        private void chnage1(String str,boolean isChecked){
+            if (isChecked){
+                stringBuilder.append(str);
+                Log.i(TAG, "onCheckedChanged:"+stringBuilder);
+            }else{
+                int start = stringBuilder.indexOf(str);
+                if(start!=-1){
+                    int end = str.length()+start;
+                    stringBuilder.delete(start,end);
+                }
+
+            }
         }
     }
+
+
 }
